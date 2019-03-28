dd19c0135f3570c09823b20201e4cb321eab8e93
Joran Chalal <joran.chalal@gmail.com>
2019-03-28 14:01:01 +0100

    Create forms.py

---
+++ RedPill/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class LocationSearchForm(forms.Form):
+    city = forms.CharField(label="City name", max_length=100)
+    postal_code = forms.IntegerField(label="Postal code")
+    real_estate_type = forms.IntegerField(label="Real estate type")
+    furnished = forms.BooleanField(label="Is furnished")
+    min_price = forms.IntegerField(label="Minimum price")
+    max_price = forms.IntegerField(label="Maximum price")
+    min_rooms = forms.IntegerField(label="Minimum rooms")
+    max_rooms = forms.IntegerField(label="Maximum rooms")
+    min_square = forms.FloatField(label="Minimum square")
+    max_square = forms.FloatField(label="Maximum square")
